8f3e51c07c9468fd20db3133bf8a4a8a6d397a9e
bouix <bouix.dev@gmail.com>
2026-09-10 14:04:20 -0300

    feat: añadir AOS como dependencia y optimizar fuentes en el proyecto

--- src/scripts/aos.ts
+++ src/scripts/aos.ts
@@ -1,19 +1,6 @@
 // AOS Animation Initialization
-interface AOSOptions {
-  offset: number;
-  delay: number;
-  duration: number;
-  easing: string;
-  once: boolean;
-  mirror: boolean;
-  anchorPlacement: string;
-}
-
-interface AOS {
-  init: (options: AOSOptions) => void;
-}
-
-declare const AOS: AOS;
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export function initAOS(): void {
   AOS.init({
